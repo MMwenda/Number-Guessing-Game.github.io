9483f817777f6285bb700cda7e309dd4eaca34a2
MMwenda <161514002+MMwenda@users.noreply.github.com>
2024-06-03 15:14:10 +0300

    First Commit

---
+++ index1.js
@@ -0,0 +1,31 @@
+const myLabel = document.getElementById("myLabel");
+let myBtn = document.getElementById("myBtn");
+let myInput = document.getElementById("myInput");
+const textArea = document.getElementById("myText");
+const myWin = document.getElementById("myWin");
+
+const minNum = 1;
+const maxNum = 100;
+let answer =  Math.floor(Math.random() *(maxNum - minNum + 1)) + minNum;
+
+let attempts = 0;
+// let guess = -1; // initialize guess to a number outside the valid range
+
+myBtn.onclick = function() {
+  guess = Number(myInput.value);
+
+  if(isNaN(guess) || guess < minNum || guess > maxNum) {
+    textArea.innerHTML = `Please enter a valid number between ${minNum} and ${maxNum}.`;
+  } else {
+    attempts++;
+    textArea.innerHTML = ""; // clear any previous messages
+    if(guess < answer) {
+      textArea.innerHTML = `Too low, try again.`;
+    } else if (guess > answer) {
+      textArea.innerHTML = `Too high, try again.`;
+    } else {
+      myWin.innerHTML = `CORRECT! The answer was ${answer}. It took you ${attempts} attempts.`;
+      myBtn.disabled = true; // disable the button to end the game
+    }
+  }
+};
